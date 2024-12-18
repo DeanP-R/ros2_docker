2ef21bddd33432b71facdf8d6fb2d9cf7f153322
DeanP-R <Deanrowlett00@gmail.com>
2024-12-18 12:45:01 +0000

    build bug fix

--- build.bash
+++ build.bash
@@ -5,7 +5,7 @@ image_name=ros2_docker_image
 image_plus_tag=$image_name:latest
 
 # Build the Docker image
-docker build --build-arg -t $image_plus_tag -f Dockerfile .
+docker build -t $image_plus_tag -f Dockerfile .
 
 # Instructions
 echo "Built $image_plus_tag and tagged as $image_name:latest"
